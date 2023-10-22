5bacc06fce3f53a5fb36f9ee1025ebfd93efbf8f
Roman <budarin.romain@yandex.ru>
2023-10-22 19:35:14 +0300

    lat и lon

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <React.StrictMode>
       <App />
-    </React.StrictMode>,
+    </React.StrictMode>
   </QueryClientProvider>
 );
