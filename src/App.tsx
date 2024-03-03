e44ddd74835ca13cbd4a06cfe58f06744e89ba6c
PenguinSatanJr <penguinsatan107@gmail.com>
2024-03-03 20:40:22 +0400

    latest changes applied

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,64 @@
 import PetsIcon from '@mui/icons-material/Pets';
-import { Container, Dialog, DialogTitle, IconButton, Stack } from '@mui/material';
+import { Container, IconButton, List, Stack, TextField } from '@mui/material';
 import { useState } from 'react';
+import ListItem from './list-item';
 
+// type WeatherQueryParam = {
+//   API_KEY: string;
+//   lat: string;
+//   lon: string;
+//   units: string;
+// };
 
-type WeatherQueryParam = {
-  API_KEY: string;
-  lat: string;
-  lon: string;
-  units: string;
-};
+// const queryParams: WeatherQueryParam = {
+//   API_KEY: "39612356bfcb87e2061a89eb187b5f9c",
+//   lat: "40.8141",
+//   lon: "14.3391",
+//   units: "metric",
+// };
 
-const queryParams: WeatherQueryParam = {
-  API_KEY: "39612356bfcb87e2061a89eb187b5f9c",
-  lat: "40.8141",
-  lon: "14.3391",
-  units: "metric",
-};
+// type Person = {
+//   name: string;
+//   surname: string;
+//   age: number;
+// };
 
-type Person = {
-  name: string;
-  surname: string;
-  age: number;
-};
+// const ivan = {
+//   name: "Ivan",
+//   surname: "Garbuz",
+//   age: 32,
+// };
 
-const ivan = {
-  name: "Ivan",
-  surname: "Garbuz",
-  age: 32,
-};
+// const ivanFavoriteNumbers = [7, 3, 12, 666, 1488];
 
-const ivanFavoriteNumbers = [7, 3, 12, 666, 1488];
-
-const greetingWithFavoriteNumbers = (person: Person, numbers: number[]) =>
-  `Hello my name is ${person.name} ${person.surname}, I am ${person.age} years old. My favorite numbers is ${numbers}`;
+// const greetingWithFavoriteNumbers = (person: Person, numbers: number[]) =>
+//   `Hello my name is ${person.name} ${person.surname}, I am ${person.age} years old. My favorite numbers is ${numbers}`;
 
 
 const App = () => {
+  const [input, setInput] = useState('');
 
-  const [open, setOpen] = useState(false);
-
+  const [items, setItems] = useState<string[]>([]);
 
   return (
-    <Container maxWidth={'xl'}>
-      <Stack spacing={1}>
-        <IconButton sx={{ color: 'purple' }} onClick={() => setOpen(true)}>
+    <Container maxWidth={'sm'} disableGutters>
+      <Stack direction={'row'} sx={{ paddingTop: '150px' }} justifyContent={'center'}>
+        <TextField
+          label={'Enter some items to buy'}
+          fullWidth
+          onChange={({ target: { value } }) => setInput(value)
+          }
+          value={input}
+        />
+        <IconButton sx={{ color: 'purple', width: "150px" }} onClick={() =>
+          input.trim().length && setItems([...items, input.trim()])}>
           <PetsIcon fontSize='large' />
         </IconButton>
       </Stack>
-      <Dialog onClose={() => setOpen(false)} open={open}>
-        <DialogTitle>Hello</DialogTitle>
-      </Dialog>
+      <List>
+        {items.map((value, index) => (<ListItem item={value} key={index} />))}
+      </List>
     </Container>
   );
 };
-
 export default App;
---
+++ src/list-item.tsx
@@ -0,0 +1,25 @@
+import { Checkbox, Typography, IconButton, ListItem as MuiListItem } from "@mui/material"
+import EditIcon from '@mui/icons-material/Edit';
+import ClearIcon from '@mui/icons-material/Clear';
+
+type ListItemProps = {
+    item: string;
+};
+
+const ListItem = ({ item }: ListItemProps) =>
+    <MuiListItem divider >
+        <Checkbox />
+        <Typography>
+            {item}
+        </Typography>
+        <IconButton >
+            <EditIcon fontSize={'small'} />
+        </IconButton>
+        <IconButton >
+            <ClearIcon fontSize={'small'} />
+        </IconButton>
+    </MuiListItem>;
+
+
+
+export default ListItem;
